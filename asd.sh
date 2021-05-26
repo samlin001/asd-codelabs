3400d1b12f26dad0841de35cd88c7fad110eb6ea
Sam Lin <samlin001@gmail.com>
2021-05-25 21:39:47 -0700

    Add asd.sh

---
+++ asd.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+readme() {
+echo '''
+Utility scripts for Android System Development, e.g.
+$ ./asd.sh setupVm
+$ ./asd.sh buildSdkPhone
+$ ./asd.sh buildAPhone
+$ ./asd.sh runAvd
+$ ./asd.sh avdInfo
+'''
+}
+
+setupVm() {
+  echo "Check if kvm is enabled for the VM. If not, follow Enabling nested virtualization for VM instances"
+  ls -l /dev/kvm
+  echo "Add yourself to the kvm grols up"
+  sudo adduser ${USER} kvm
+  echo "Ensure the kvm group can access to kvm"
+  sudo chmod 660 /dev/kvm
+  grep kvm /etc/group
+
+  echo "link ${HOME}/Android to /ws/Android to use preloaded SDK"
+  ln -s /ws/Android ${HOME}/Android
+}
+
+studio() {
+  /ws/android-studio/bin/studio.sh &
+}
+
+build() {
+  SECONDS=0
+  m -j16
+  echo "took ${SECONDS} sec. to build"
+}
+
+buildSdkPhone() {
+  . build/envsetup.sh && lunch sdk_phone_x86_64-userdebug
+  build
+}
+
+buildAPhone() {
+  . build/envsetup.sh && lunch aphone-userdebug
+  build
+}
+
+runAvd() {
+  emulator -no-snapshot -memory 4096 -qemu -smp 6 &
+}
+
+avdInfo()
+{
+  echo "Check key device info"
+  adb shell getprop | grep ro.build.fingerprint
+  adb shell getprop | grep abi
+  adb shell ls -l /product/media
+  adb shell ls -l /product/app
+  adb shell pm list features
+  adb shell cat /proc/meminfo
+  adb shell cat /proc/cpuinfo
+}
+
+if [[ -z $1 ]]; then
+    readme
+else
+    echo "Running: $@"
+    $@
+fi
