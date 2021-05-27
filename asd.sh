ee30a7e493680abfbb7380d6fba152a50e112181
Sam Lin <samlin001@gmail.com>
2021-05-26 18:32:41 -0700

    Add setEnv

--- asd.sh
+++ asd.sh
@@ -4,7 +4,9 @@ readme() {
 echo '''
 Utility scripts for Android System Development, e.g.
 ./asd.sh setupVm
+. ./asd.sh setEnv
 ./asd.sh studio
+./asd.sh sync
 ./asd.sh buildSdkPhone
 ./asd.sh buildAPhone
 ./asd.sh runAvd
@@ -13,6 +15,8 @@ Utility scripts for Android System Development, e.g.
 }
 
 setupVm() {
+  SECONDS=0
+  echo "Set up a new VM"
   echo "Check if kvm is enabled for the VM. If not, follow Enabling nested virtualization for VM instances"
   ls -l /dev/kvm
   echo "Add yourself to the kvm grols up"
@@ -25,24 +29,49 @@ setupVm() {
 
   echo "link ${HOME}/Android to /ws/Android to use preloaded SDK"
   ln -s /ws/Android ${HOME}/Android
+  echo "setupVm took ${SECONDS} s."
 }
+
+setEnv() {
+  echo "Set up development environment for each terminal."
+  if [[ -z ${ANDROID_BUILD_TOP} ]]; then
+    export ANDROID_BUILD_TOP="/ws/android"
+  fi
+  echo "ANDROID_BUILD_TOP=${ANDROID_BUILD_TOP}"
+  if [[ -z ${ANDROID_SDK_ROOT} ]]; then
+    export ANDROID_SDK_ROOT="${HOME}/Android/Sdk"
+  fi
+  export PATH=$PATH:/ws/bin:/ws/asd-codelabs
+  alias lunchSdkPhone . build/envsetup.sh && lunch sdk_phone_x86_64-userdebug
+  alias lunchAPhone . build/envsetup.sh && lunch aphone-userdebug
+}
+
 
 studio() {
   /ws/android-studio/bin/studio.sh &
 }
 
+sync() {
+  cd "${ANDROID_BUILD_TOP}"
+  SECONDS=0
+  repo sync -j8
+  echo "took ${SECONDS} sec. to sync."
+}
+
 build() {
   SECONDS=0
   m -j16
-  echo "took ${SECONDS} sec. to build"
+  echo "took ${SECONDS} sec. to build."
 }
 
 buildSdkPhone() {
+  cd "${ANDROID_BUILD_TOP}"
   . build/envsetup.sh && lunch sdk_phone_x86_64-userdebug
   build
 }
 
 buildAPhone() {
+  cd "${ANDROID_BUILD_TOP}"
   . build/envsetup.sh && lunch aphone-userdebug
   build
 }
